b111d604bbd847c4f6dfc87266a6b40e50f4ca08
Suraj Rao <138556273+chocolatesuraj@users.noreply.github.com>
2024-09-13 22:41:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,44 @@ from PIL import Image
 import pytesseract
 import src.utils
 
+entity_unit_map = {
+    'width': {'centimetre', 'foot', 'inch', 'metre', 'millimetre', 'yard'},
+    'depth': {'centimetre', 'foot', 'inch', 'metre', 'millimetre', 'yard'},
+    'height': {'centimetre', 'foot', 'inch', 'metre', 'millimetre', 'yard'},
+    'item_weight': {'gram',
+        'kilogram',
+        'microgram',
+        'milligram',
+        'ounce',
+        'pound',
+        'ton'},
+    'maximum_weight_recommendation': {'gram',
+        'kilogram',
+        'microgram',
+        'milligram',
+        'ounce',
+        'pound',
+        'ton'},
+    'voltage': {'kilovolt', 'millivolt', 'volt'},
+    'wattage': {'kilowatt', 'watt'},
+    'item_volume': {'centilitre',
+        'cubic foot',
+        'cubic inch',
+        'cup',
+        'decilitre',
+        'fluid ounce',
+        'gallon',
+        'imperial gallon',
+        'litre',
+        'microlitre',
+        'millilitre',
+        'pint',
+        'quart'}
+}
+
+from transformers import pipeline
+pipe = pipeline("question-answering", model="M-FAC/bert-mini-finetuned-squadv2")
+
 pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
 
 def predictor(image_link, category_id, entity_name):
@@ -30,6 +68,10 @@ def predictor(image_link, category_id, entity_name):
         image = Image.open(image_save_path)
         text_in_image = pytesseract.image_to_string(image)
         print("texttttttttt",text_in_image)
+        question= f'what is the {entity_name} the allowed untits are {entity_unit_map[entity_name]} give the answer as  one number followed by the unit only'
+        print(f"Question: {question}")
+        answer = pipe(question= question , context= text_in_image)
+        print(f"Answer: {answer['answer']}")
     
     except Exception as e:
         print(f"Error processing image: {image_link}. Error: {str(e)}")
